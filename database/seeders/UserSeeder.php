7f09b48e3653cab49cdb3999799425220fe20da2
Adam Masha <adamngowa3@gmail.com>
2025-10-04 18:20:20 +0300

    only admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,9 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-use App\Models\Vendor;
 use App\Enums\RolesEnum;
-use App\Enums\VendorStatusEnum;
 
 class UserSeeder extends Seeder
 {
@@ -16,44 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Normal User
-        User::factory()->create([
-            'name' => 'User',
-            'email' => 'user@example.com',
-            'password' => Hash::make('12345678'),
-        ])->assignRole(RolesEnum::User->value);
-
-        // Vendor (already approved)
-        $user = User::factory()->create([
-            'name' => 'Vendor',
-            'email' => 'vendor@example.com',
-            'password' => Hash::make('12345678'),
-        ]);
-        $user->assignRole(RolesEnum::Vendor->value);
-
-        Vendor::factory()->create([
-            'user_id' => $user->id,
-            'status' => VendorStatusEnum::Approved,
-            'store_name' => 'Vendor Store',
-            'store_address' => '123 vendor street',
-        ]);
-
-        // Vendor 2
-        $user = User::factory()->create([
-            'name' => 'Vendor 2',
-            'email' => 'vendor2@example.com',
-            'password' => Hash::make('12345678'),
-        ]);
-        $user->assignRole(RolesEnum::Vendor->value);
-
-        Vendor::factory()->create([
-            'user_id' => $user->id,
-            'status' => VendorStatusEnum::Approved,
-            'store_name' => 'Vendor 2 Store',
-            'store_address' => '123 vendor street',
-        ]);
-
-        // Admin
+        // Admin only
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'adamngowa3@gmail.com',
