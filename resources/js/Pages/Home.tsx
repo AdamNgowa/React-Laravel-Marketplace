484f4439c774b9c873c59bb0151b435bd554795d
Adam Masha <adamngowa3@gmail.com>
2025-10-05 17:10:55 +0300

    hero

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -14,9 +14,8 @@ export default function Home({
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Hello there</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Welcome to our store! Explore our wide range of products and find
+              what you need.
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
